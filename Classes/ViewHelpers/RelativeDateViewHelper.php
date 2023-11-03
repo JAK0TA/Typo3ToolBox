390c518378f2b57476d8e97d9bbfb6c060dfbfca
Michael Krohn <krohn@jakota.de>
2023-11-03 10:09:53 +0100

    refactor: move relative date calculation to service class

--- Classes/ViewHelpers/RelativeDateViewHelper.php
+++ Classes/ViewHelpers/RelativeDateViewHelper.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 
 namespace JAKOTA\Typo3ToolBox\ViewHelpers;
 
+use JAKOTA\Typo3ToolBox\Utility\DateUtility;
 use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
 
 class RelativeDateViewHelper extends AbstractViewHelper {
@@ -22,30 +23,7 @@ class RelativeDateViewHelper extends AbstractViewHelper {
   public function render() {
     /** @var \DateTime $date */
     $date = $this->arguments['value'];
-    $timestamp = $date->getTimestamp();
-    $currentTime = time();
 
-    $strTime = ['Sekunde', 'Minute', 'Stunde', 'Tag', 'Monat', 'Jahr'];
-    $length = [60, 60, 24, 30, 12, 10];
-
-    if ($currentTime >= $timestamp) {
-      $diff = time() - $timestamp;
-      for ($i = 0; $diff >= $length[$i] && $i < count($length) - 1; ++$i) {
-        $diff = $diff / $length[$i];
-      }
-      $diff = round($diff);
-      $out = 'vor '.$diff.' '.$strTime[$i];
-      if ($diff > 1) {
-        if ('e' == $strTime[$i][-1]) {
-          $out = $out.'n';
-        } else {
-          $out = $out.'en';
-        }
-      }
-
-      return $out;
-    }
-
-    return null;
+    return DateUtility::calculateRelativeDate($date);
   }
 }
