bd0979cf29b204ffcf7a6f4e11f9a2643b444e9b
Thomas Lüder <t.lueder@nets.de>
2024-02-19 08:36:12 +0800

    Added option to create XML response

---
+++ Classes/Definition/ContentTypeDefinition.php
@@ -0,0 +1,14 @@
+<?php
+
+// Copyright JAKOTA Design Group GmbH. All rights reserved.
+declare(strict_types=1);
+
+namespace JAKOTA\Typo3ToolBox\Definition;
+
+class ContentTypeDefinition {
+  public const DEFAULT = 'default';
+
+  public const JSON = 'json';
+
+  public const XML = 'xml';
+}
