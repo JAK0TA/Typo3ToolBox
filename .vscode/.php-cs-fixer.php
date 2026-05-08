54a90fc548ea298164ae48e731b9c7ff85b159b7
Nicklas Mantek <mantek@jakota.de>
2026-05-09 00:04:53 +0200

    pin to version 3

--- .vscode/.php-cs-fixer.php
+++ .vscode/.php-cs-fixer.php
@@ -1,46 +1,24 @@
 <?php
 
+use PhpCsFixer\Config;
+use PhpCsFixer\Finder;
+
 // liste der Regeln: https://github.com/FriendsOfPHP/PHP-CS-Fixer/blob/master/doc/rules/index.rst
 
-$finder = PhpCsFixer\Finder::create()
+$finder = Finder::create()
   ->exclude('vendor')
   ->exclude('Vendor')
   ->ignoreVCSIgnored(true)
 ;
 
-$toolInfo = new PhpCsFixer\ToolInfo();
-
-$csFixerVersion = $toolInfo->getVersion();
-if (false !== stripos($csFixerVersion, ':')) {
-  $csFixerVersion = substr($csFixerVersion, 0, stripos($csFixerVersion, ':'));
-}
-
-if (version_compare($csFixerVersion, '3.9.0', '<')) {
-  $rules = [
+return (new Config())
+  ->setRules([
     '@PhpCsFixer' => true,
-    'braces' => [
-      'position_after_functions_and_oop_constructs' => 'same',
+    'curly_braces_position' => [
+      'control_structures_opening_brace' => 'same_line',
+      'classes_opening_brace' => 'same_line',
+      'functions_opening_brace' => 'same_line',
     ],
-    'no_whitespace_before_comma_in_array' => true,
-    'whitespace_after_comma_in_array' => true,
-    'ordered_class_elements' => [
-      'sort_algorithm' => 'alpha',
-    ],
-    'ordered_imports' => [
-      'sort_algorithm' => 'alpha',
-    ],
-    'class_attributes_separation' => [
-      'elements' => [
-        'const' => 'one',
-        'method' => 'one',
-        'property' => 'one',
-        'trait_import' => 'one',
-      ],
-    ],
-  ];
-} else {
-  $rules = [
-    '@PhpCsFixer' => true,
     'braces' => [
       'position_after_functions_and_oop_constructs' => 'same',
     ],
@@ -61,11 +39,7 @@ if (version_compare($csFixerVersion, '3.9.0', '<')) {
         'trait_import' => 'one',
       ],
     ],
-  ];
-}
-
-return (new PhpCsFixer\Config())
-  ->setRules($rules)
+  ])
   ->setFinder($finder)
   ->setLineEnding("\n")
   ->setIndent('  ')
