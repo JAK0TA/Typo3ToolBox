a4eca14546c2e7f3a98d1875a1611a2f5bada1eb
Nicklas Mantek <mantek@jakota.de>
2026-05-09 00:05:38 +0200

    init typoscript in middleware

--- Classes/Middleware/MiddlewareActionAbstract.php
+++ Classes/Middleware/MiddlewareActionAbstract.php
@@ -10,20 +10,19 @@ use Psr\Http\Message\UriInterface;
 use TYPO3\CMS\Core\Context\Context;
 use TYPO3\CMS\Core\Context\LanguageAspect;
 use TYPO3\CMS\Core\Context\UserAspect;
-use TYPO3\CMS\Core\Information\Typo3Version;
 use TYPO3\CMS\Core\Imaging\ImageManipulation\CropVariantCollection;
 use TYPO3\CMS\Core\Localization\LanguageService;
 use TYPO3\CMS\Core\Localization\LanguageServiceFactory;
-use TYPO3\CMS\Core\Routing\PageArguments;
 use TYPO3\CMS\Core\Site\Entity\Site;
 use TYPO3\CMS\Core\Site\Entity\SiteLanguage;
-use TYPO3\CMS\Core\TypoScript\TemplateService;
+use TYPO3\CMS\Core\TypoScript\AST\Node\RootNode;
+use TYPO3\CMS\Core\TypoScript\FrontendTypoScript;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Extbase\Configuration\BackendConfigurationManager;
 use TYPO3\CMS\Extbase\Domain\Model\FileReference;
-use TYPO3\CMS\Extbase\Service\ImageService;
 use TYPO3\CMS\Extbase\Mvc\Web\Routing\UriBuilder;
+use TYPO3\CMS\Extbase\Service\ImageService;
 use TYPO3\CMS\Frontend\Authentication\FrontendUserAuthentication;
-use TYPO3\CMS\Frontend\Controller\TypoScriptFrontendController;
 
 abstract class MiddlewareActionAbstract extends ApiAbstract {
   protected string $languageFile = '';
@@ -98,44 +97,76 @@ abstract class MiddlewareActionAbstract extends ApiAbstract {
       $GLOBALS['LANG'] = $this->languageService;
     }
 
-    $typo3Version = GeneralUtility::makeInstance(Typo3Version::class);
-    if (version_compare($typo3Version->getVersion(), '13.4.0') < 0) {
-      if (!isset($GLOBALS['TSFE'])) {
-        $parsedBody = (array) $this->request->getParsedBody();
+    $backendConfigurationManager = GeneralUtility::makeInstance(
+      BackendConfigurationManager::class
+    );
 
-        $GLOBALS['TSFE'] = GeneralUtility::makeInstance(
-          TypoScriptFrontendController::class,
-          $context,
-          $this->site,
-          $this->siteLanguage,
-          new PageArguments(
-            intval($this->queryParams['id'] ?? $parsedBody['id'] ?? $this->site?->getRootPageId() ?? 0),
-            strval($this->queryParams['type'] ?? $parsedBody['type'] ?? ''),
-            [],
-            $this->queryParams
-          ),
-          $frontendUser,
-        );
+    $typoScript = new FrontendTypoScript(GeneralUtility::makeInstance(RootNode::class), [], [], []);
+    $typoScript->setSetupArray($backendConfigurationManager->getTypoScriptSetup($this->request));
 
-        $GLOBALS['TSFE']->tmpl = GeneralUtility::makeInstance(TemplateService::class);
-        $GLOBALS['TSFE']->determineId($this->request);
-        $GLOBALS['TSFE']->getConfigArray();
-        $GLOBALS['TSFE']->newCObj($this->request);
-      }
+    $frontendTypoScript = $this->request->getAttribute('frontend.typoscript');
+    if (null === $frontendTypoScript || !$frontendTypoScript->hasSetup()) {
+      $this->request = $this->request->withAttribute('frontend.typoscript', $typoScript);
+      $GLOBALS['TYPO3_REQUEST'] = $this->request;
     }
 
     $this->uriBuilder = GeneralUtility::makeInstance(UriBuilder::class);
   }
 
+  public function resizeAndCropImage(?FileReference $image, string $width = '2480', string $height = '770', string $cropArea = 'default', bool $absUrl = false): ?string {
+    if (null == $image) {
+      return null;
+    }
+
+    if ('svg' == strtolower($image->getOriginalResource()->getExtension())) {
+      if ($absUrl) {
+        if (null == $this->siteLanguage) {
+          return '/'.$image->getOriginalResource()->getPublicUrl();
+        }
+
+        return $this->siteLanguage->getBase()->getScheme().'://'.$this->siteLanguage->getBase()->getHost().'/'.$image->getOriginalResource()->getPublicUrl();
+      }
+
+      return '/'.$image->getOriginalResource()->getPublicUrl();
+    }
+
+    $imageService = GeneralUtility::makeInstance(ImageService::class);
+    $getImage = $imageService->getImage('', $image, false);
+
+    $cropString = null;
+    if ($getImage->hasProperty('crop') && $getImage->getProperty('crop')) {
+      $cropString = $getImage->getProperty('crop');
+    }
+
+    $cropVariantCollection = CropVariantCollection::create(strval($cropString));
+    $cropArea = $cropVariantCollection->getCropArea($cropArea);
+
+    $processedImage = $imageService->applyProcessingInstructions($getImage, [
+      'width' => $width,
+      'height' => $height,
+      'crop' => $cropArea->isEmpty() ? null : $cropArea->makeAbsoluteBasedOnFile($getImage),
+    ]);
+
+    if ($absUrl) {
+      if (null == $this->siteLanguage) {
+        return '/'.$processedImage->getPublicUrl();
+      }
+
+      return $this->siteLanguage->getBase()->getScheme().'://'.$this->siteLanguage->getBase()->getHost().'/'.$processedImage->getPublicUrl();
+    }
+
+    return '/'.$processedImage->getPublicUrl();
+  }
+
   /**
    * Function to build urls without using $this->uriBuilder.
    *
-   * @param array<string, mixed>  $additionalGetParams additional GET parameters
-   * @param null|string           $extensionName       Extension name without underscores. Eg. 'myextension'
-   * @param null|string           $pluginName          Plugin name with underscores. Eg. 'news_show' or 'records_list'
-   * @param null|string           $actionName          Action name (the 'show' of 'news_show')
-   * @param null|string           $controllerName      Name of the controller. Eg. 'News' or 'Record'
-   * @param array<string, mixed>  $actionArguments     Additional arguments needed for the Action. Eg. [newsId => 123, ...]
+   * @param array<string, mixed> $additionalGetParams additional GET parameters
+   * @param null|string          $extensionName       Extension name without underscores. Eg. 'myextension'
+   * @param null|string          $pluginName          Plugin name with underscores. Eg. 'news_show' or 'records_list'
+   * @param null|string          $actionName          Action name (the 'show' of 'news_show')
+   * @param null|string          $controllerName      Name of the controller. Eg. 'News' or 'Record'
+   * @param array<string, mixed> $actionArguments     Additional arguments needed for the Action. Eg. [newsId => 123, ...]
    */
   protected function buildUri(int $pageId, array $additionalGetParams = [], ?string $extensionName = null, ?string $pluginName = null, ?string $actionName = null, ?string $controllerName = null, array $actionArguments = []): ?UriInterface {
     if (null === ($this->site?->getRouter() ?? null)) {
@@ -188,50 +219,5 @@ abstract class MiddlewareActionAbstract extends ApiAbstract {
     }
 
     return $keyToTranslate;
-  }
-
-  public function resizeAndCropImage(?FileReference $image, string $width = '2480', string $height = '770', string $cropArea = 'default', bool $absUrl = false): ?string {
-    if (null == $image) {
-      return null;
-    }
-
-    if ('svg' == strtolower($image->getOriginalResource()->getExtension())) {
-      if ($absUrl) {
-        if (null == $this->siteLanguage) {
-          return '/'.$image->getOriginalResource()->getPublicUrl();
-        }
-
-        return $this->siteLanguage->getBase()->getScheme().'://'.$this->siteLanguage->getBase()->getHost().'/'.$image->getOriginalResource()->getPublicUrl();
-      }
-
-      return '/'.$image->getOriginalResource()->getPublicUrl();
-    }
-
-    $imageService = GeneralUtility::makeInstance(ImageService::class);
-    $getImage = $imageService->getImage('', $image, false);
-
-    $cropString = null;
-    if ($getImage->hasProperty('crop') && $getImage->getProperty('crop')) {
-      $cropString = $getImage->getProperty('crop');
-    }
-
-    $cropVariantCollection = CropVariantCollection::create(strval($cropString));
-    $cropArea = $cropVariantCollection->getCropArea($cropArea);
-
-    $processedImage = $imageService->applyProcessingInstructions($getImage, [
-      'width' => $width,
-      'height' => $height,
-      'crop' => $cropArea->isEmpty() ? null : $cropArea->makeAbsoluteBasedOnFile($getImage),
-    ]);
-
-    if ($absUrl) {
-      if (null == $this->siteLanguage) {
-        return '/'.$processedImage->getPublicUrl();
-      }
-
-      return $this->siteLanguage->getBase()->getScheme().'://'.$this->siteLanguage->getBase()->getHost().'/'.$processedImage->getPublicUrl();
-    }
-
-    return '/'.$processedImage->getPublicUrl();
   }
 }
